4c850ee9a1db35fd693359f1e3f5dfa138ce8d0a
Michael de Silva <michael@mwdesilva.com>
2017-10-25 17:21:32 +0530

    Improved example

--- main.cpp
+++ main.cpp
@@ -2,16 +2,19 @@
 #include "src/core/Cog.h"
 
 int main() {
-
     auto component = std::make_shared<Component>();
     auto cog = std::make_shared<Cog>();
 
     bool componentChanges = component->anyChange();
+    std::cout << "Component's changes: " << componentChanges << std::endl;
+
+    bool cogChanges = cog->changes();
+    std::cout << "Cog's (original) changes: " << cogChanges << std::endl;
 
     cog->setChange(true);
-    bool cogChanges = cog->changes();
+    cogChanges = cog->changes();
 
-    std::cout << "Component's changes: " << componentChanges << std::endl;
     std::cout << "Cog's changes: " << cogChanges << std::endl;
+
     return 0;
 }
